881dcdc718d38b8f2e55742dc34d5170f70b52bb
Michael GENA <mgena@nuxeo.com>
2016-06-16 19:13:22 +0200

    How to import a file with the java client

--- src/main/java/client/nuxeo/ImportFile.java
+++ src/main/java/client/nuxeo/ImportFile.java
@@ -1,6 +1,9 @@
 package client.nuxeo;
+import java.io.File;
+
 import org.nuxeo.client.api.NuxeoClient;
 import org.nuxeo.client.api.objects.Document;
+import org.nuxeo.client.api.objects.upload.BatchUpload;
 
 /**
  * Created by mgena on 6/16/16.
@@ -11,8 +14,18 @@ public class ImportFile {
 	    NuxeoClient nuxeoClient = new NuxeoClient(url, "Administrator", "Administrator");
 	    
 	    // Create a document
-	    Document document = new Document("file", "File");
+	    Document document = new Document("document1", "File");
 	    document.set("dc:title", "new title");
 	    document = nuxeoClient.repository().createDocumentByPath("/default-domain/workspaces/java", document);
+	    
+	    // Upload the file
+	    BatchUpload batchUpload = nuxeoClient.fetchUploadManager().enableChunk();
+	    File file = new File("/Users/mgena/Documents/BBVA/NatGeo01.jpg");
+	    batchUpload = batchUpload.upload(file.getName(), file.length(), "jpg", batchUpload.getBatchId(), "1", file);
+	    
+	    // Attach the file to the document
+	    document.set("file:content", batchUpload.getBatchBlob());
+	    document = document.updateDocument();
+
 	}
 }
